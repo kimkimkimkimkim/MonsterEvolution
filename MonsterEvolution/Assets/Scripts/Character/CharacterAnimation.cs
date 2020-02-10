5ee11e2f4619e516d0badd684a75c566e7ad4132
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2020-02-10 21:56:57 +0900

    機能追加　キャラアニメーションとセーフエリア対応

---
+++ MonsterEvolution/Assets/Scripts/Character/CharacterAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    //オブジェクト参照
+    public List<Sprite> sprites = new List<Sprite>();
+    public float span = 0.5f;
+
+    //変数
+    private float time = 0;
+    private int index = 0;
+    private int listCount = 0;
+    private Image myImage;
+
+    void Start()
+    {
+        listCount = sprites.Count;
+        myImage = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        time += Time.deltaTime;
+        if(time >= span){
+            time = 0;
+            index++;
+            if(index>=listCount)index -= listCount;
+            myImage.sprite = sprites[index];
+        }
+    }
+}
