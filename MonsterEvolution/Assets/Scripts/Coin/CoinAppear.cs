458ccb7c788d76c9ff95ec0306bd1565ceafd8d4
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2020-02-12 00:58:34 +0900

    機能追加　コイン自動生成、取得

---
+++ MonsterEvolution/Assets/Scripts/Coin/CoinAppear.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CoinAppear : MonoBehaviour
+{
+    //定数定義
+    private const int MAX_COIN_NUM = 15;
+    private const int RESPAWN_TIME = 5000; //コインの出現間隔[ms]
+
+    //オブジェクト参照
+    public GameObject coinPrefab;
+    public GameObject coinArea;
+
+    //変数宣言
+    private RectTransform coinAreaT;
+    private Vector3 coinAreaPos;
+    private float coinAreaWidth;
+    private float coinAreaHeight;
+    private float padding = 7;
+    private int currentCoinNum = 0; //現在のコイン数
+    private DateTime lastDateTime; //前回コインを生成した時間
+
+    void Start()
+    {
+        coinAreaT = coinArea.GetComponent<RectTransform>();
+        coinAreaPos = new Vector3(coinAreaT.rect.x,coinAreaT.rect.y,0);
+        coinAreaWidth = coinAreaT.rect.width;
+        coinAreaHeight = coinAreaT.rect.height;
+
+        //初期コイン生成
+        currentCoinNum = 10;
+        for(int i=0;i<currentCoinNum;i++){
+            Appear();
+        }
+        //初期設定
+        lastDateTime = DateTime.UtcNow;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentCoinNum < MAX_COIN_NUM){
+            TimeSpan timeSpan = DateTime.UtcNow - lastDateTime;
+
+            if(timeSpan >= TimeSpan.FromMilliseconds(RESPAWN_TIME)){
+                while(timeSpan >= TimeSpan.FromMilliseconds(RESPAWN_TIME)){
+                    AppearNewCoin();
+                    timeSpan -= TimeSpan.FromMilliseconds(RESPAWN_TIME);
+                }
+            }
+        }
+    }
+
+    //新しいコインを出現させる
+    void AppearNewCoin(){
+        lastDateTime = DateTime.UtcNow;
+        if(currentCoinNum >= MAX_COIN_NUM) return;
+        Appear();
+        currentCoinNum++;
+    }
+
+    //コインを出現させる
+    void Appear(){
+        Vector3 pos = new Vector3(0,0,0);
+        GameObject coin = (GameObject)Instantiate(coinPrefab);
+        coin.transform.SetParent(coinArea.transform,false);
+        coin.transform.localScale = new Vector3(1,1,1);
+        coin.transform.localPosition = new Vector3(
+            UnityEngine.Random.Range(coinAreaPos.x+padding,coinAreaPos.x+coinAreaWidth-padding),
+            UnityEngine.Random.Range(coinAreaPos.y+padding,coinAreaPos.y+coinAreaHeight-padding),
+            0
+        );
+    }
+
+    //コインをゲット
+    public void GetCoin(){
+        currentCoinNum--;
+    }
+}
---
+++ MonsterEvolution/Assets/Scripts/Coin/CoinManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    //変数宣言
+    private GameObject coinArea;
+
+    void Start(){
+        coinArea = GameObject.Find("Canvas/CoinArea");
+    }
+
+    public void TouchCoin(){
+        if(Input.GetMouseButton(0) == false) return;
+        
+        coinArea.GetComponent<CoinAppear>().GetCoin();
+        Destroy(this.gameObject);
+    }
+}
